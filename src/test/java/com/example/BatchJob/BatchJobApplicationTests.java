fcf2c39a2d68b67a8ff526e24ec4ebe35c979462
sachinkumar <sachinkumar5035@gmail.com>
2025-11-11 00:36:51 +0530

    added new files

---
+++ src/test/java/com/example/BatchJob/BatchJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BatchJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
