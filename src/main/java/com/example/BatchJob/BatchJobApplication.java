a65174841d44a3fe5013ffe70f98fe3d86893859
sachinkumar <sachinkumar5035@gmail.com>
2025-11-11 23:05:14 +0530

    application started

--- src/main/java/com/example/BatchJob/BatchJobApplication.java
+++ src/main/java/com/example/BatchJob/BatchJobApplication.java
@@ -8,6 +8,7 @@ public class BatchJobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchJobApplication.class, args);
+        System.out.println("application started");
 	}
 
 }
