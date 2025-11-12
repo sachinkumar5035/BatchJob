7808698e62e052b58912a8d86760953fbf043c91
sachinkumar <sachinkumar5035@gmail.com>
2025-11-12 10:41:05 +0530

    added jobController

--- src/main/java/com/example/BatchJob/BatchJobApplication.java
+++ src/main/java/com/example/BatchJob/BatchJobApplication.java
@@ -1,9 +1,11 @@
 package com.example.BatchJob;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchJobApplication {
 
 	public static void main(String[] args) {
